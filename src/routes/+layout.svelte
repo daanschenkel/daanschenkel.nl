ca648007c789ab9f3c18f0bcfd25b03cf1828e59
danny <20974756+daanschenkel@users.noreply.github.com>
2023-09-24 22:23:39 +0200

    clear next

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import '../app.postcss';
-</script>
-
-<div class="w-screen h-screen flex flex-col bg-black">
-	<slot />
-</div>
--- src/routes/sounds/+page.server.js
+++
@@ -1,4 +0,0 @@
-export async function load(){
-    let response = await fetch("https://api.lanyard.rest/v1/users/654390669472694284").then(res => res.json());
-    return response;
-}
