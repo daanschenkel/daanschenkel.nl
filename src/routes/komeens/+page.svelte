23bc82133346c9c0365bbf464493c5862104b4e9
Daan <daan@daanschenkel.nl>
2023-11-23 12:11:56 +0100

    Update +page.svelte

--- src/routes/komeens/+page.svelte
+++ src/routes/komeens/+page.svelte
@@ -1,1 +1,1 @@
-<h2 class="text-white text-center text-xl min-w-screen min-h-screen flex items-center align-center">bedankt voor het komen en goeie reis terug</h2>
+<h2 class="text-white text-center text-xl min-w-screen min-h-screen flex items-center align-center">bedankt voor het klikken en goeie reis terug</h2>
